851c58a754d4fa5a42d6fccf33069a6039d43a23
dbetteb <dbetteb@yahoo.com>
2019-03-03 16:27:49 +0100

    third commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# GLUC project
+
+## Description
+
+Plotly tools to draw HbA1C variation over time.
+Filtering is used.
+
+## Inputs
+
+
+## Databasis
+
+
---
+++ main.py
@@ -0,0 +1,43 @@
+# !/usr/bin/env python3
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass, field 
+from typing import List
+from dateutil.parser import parse
+
+
+def loadgluc(filename):
+    df = pd.read_csv(filename)
+    return df['Average30Days'].values
+
+def loaddate(filename):
+    df = pd.read_csv(filename)
+    return [parse(date, dayfirst=True) for date in df['Date'].values]
+
+def convertgluc(gluc, coeff = 1/28.7, intercept = 46.7/28.7):
+    return coeff*gluc + intercept
+
+
+
+@dataclass
+class GlucConverter:
+    '''Class for conversion'''
+    name: str
+    filename: str
+    formula: List[float] = field(default_factory=lambda : [1/28.7, 46.7/28.7])
+
+    
+    def getgluc(self) -> List:
+        self.gluc = loadgluc(self.filename)
+
+    def getdate(self) -> List:
+        self.date = loaddate(self.filename)
+
+    def gethba1c(self) -> List:
+        if hasattr(self, 'gluc') == True:
+            self.hba1c = convertgluc(self.gluc, coeff = self.formula[0], intercept = self.formula[1])
+            print(f"{self.name} GlucConverter formula is {self.formula[0]}*glyc+ {self.formula[1]}")
+        else:
+            print(f"{self.name} GlucConverter has no gluc")
