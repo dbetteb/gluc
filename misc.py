107fabef200c9bf07136d49c78ae1169b3f1e1c5
dbetteb <dbetteb@yahoo.com>
2019-03-14 10:37:00 +0100

    update

---
+++ misc.py
@@ -0,0 +1,5 @@
+# !/usr/bin/env python3
+# coding: utf-8
+
+mois = ['janv','fév','mars','avril','mai','juin','juil','aout','sept','oct','nov','dec']
+monthdic = {num: mois for num, mois in zip(range(1,13), mois)}
